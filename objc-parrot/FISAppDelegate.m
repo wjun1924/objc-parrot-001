e5ce1ee8849ae5139eeeaf7c34855c8a7eb3cb24
markedwardmurray <markedwardmurray@gmail.com>
2015-07-28 23:56:29 -0400

    remove solution

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -23,45 +23,7 @@
      *  Write your code here!
      
      */
-    
-    // Code-Along I: "Squawk!"
-    
-    NSLog(@"Squawk!");
-    
-    NSString *squawk = @"squawk";
-    NSLog(@"%@", squawk);
-    
-    squawk = [squawk capitalizedString];
-    squawk = [squawk stringByAppendingString:@"!"];
-    NSLog(@"%@", squawk);
-    
-    // Code-Along II: "Dead men tell no tales!"
-    
-    NSString *deadMen = @"Dead men";
-    NSString *tell = @"tell";
-    NSString *noTales = @"no tales";
-    NSLog(@"%@ %@ %@!", deadMen, tell, noTales);
-    
-    NSMutableString *pirateParrot = [[NSMutableString alloc] init];
-    [pirateParrot appendString:deadMen];
-    NSLog(@"%@", pirateParrot);
-    [pirateParrot appendFormat:@" %@ %@!", tell, noTales];
-    NSLog(@"%@", pirateParrot);
-    
-    // Code-Along III: Iago Is Molting
-    
-    NSString *iagoLook = @"look at me";
-    NSString *iagoMolting = @"i'm molting";
-    NSLog(@"%@ %@", iagoLook, iagoMolting);
-    
-    iagoLook = [iagoLook uppercaseString];
-    iagoMolting = [iagoMolting uppercaseString];
-    NSLog(@"%@ %@", iagoLook, iagoMolting);
-    
-    
-    NSString *iagoShout = [NSString stringWithFormat:@"%@ %@!", iagoLook, iagoMolting];
-    NSLog(@"%@", iagoShout);
-    
+        
     return YES; // Don't alter this line.
 }
 
