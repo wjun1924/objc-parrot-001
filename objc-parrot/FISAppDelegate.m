5479d8375c3add9f34af208b184147ce049f82ef
wjun1924 <wjun1924@gmail.com>
2015-07-30 16:35:07 -0400

    completed parrot

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -23,6 +23,36 @@
      *  Write your code here!
      
      */
+    NSLog(@"Squawk!");
+    
+    NSString *squawk = @"squawk";
+    NSLog(@"%@", squawk);
+    
+    squawk = [squawk capitalizedString];
+    squawk = [squawk stringByAppendingString:@"!"];
+    NSLog(@"%@", squawk);
+    
+    NSString *deadMen = @"Dead men";
+    NSString *tell = @"tell";
+    NSString *noTales = @"no tales";
+    NSLog(@"%@ %@ %@!", deadMen, tell, noTales);
+    
+    NSMutableString *pirateParrot = [[NSMutableString alloc] init];
+    [pirateParrot appendString:deadMen];
+    [pirateParrot appendFormat:@" %@ %@!", tell, noTales];
+    NSLog(@"%@",pirateParrot);
+    
+    NSString *iagoLook = @"look at me";
+    NSString *iagoMolting = @"i'm molting";
+    NSLog(@"%@ %@", iagoLook, iagoMolting);
+    
+    iagoLook = [iagoLook uppercaseString];
+    iagoMolting = [iagoMolting uppercaseString];
+    
+    NSLog(@"%@ %@", iagoLook, iagoMolting);
+    
+    NSString *iagoShout = [NSString stringWithFormat:@"%@ %@!", iagoLook, iagoMolting];
+    NSLog(@"%@", iagoShout);
         
     return YES; // Don't alter this line.
 }
