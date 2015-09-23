6ebca3a2c1e1cd3d9291be0c291fe03cbaad8b07
markedwardmurray <markedwardmurray@gmail.com>
2015-09-23 15:05:03 -0400

    comment style

--- objc-parrot/FISAppDelegate.h
+++ objc-parrot/FISAppDelegate.h
@@ -1,10 +1,4 @@
-//
 //  FISAppDelegate.h
-//  objc-parrot
-//
-//  Created by iOS Staff on 7/27/15
-//  Copyright (c) 2015 The Flatiron School. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -1,17 +1,6 @@
-//
 //  FISAppDelegate.m
-//  objc-parrot
-//
-//  Created by iOS Staff on 7/27/15
-//  Copyright (c) 2015 The Flatiron School. All rights reserved.
-//
 
 #import "FISAppDelegate.h"
-
-@interface FISAppDelegate ()
-
-@end
-
 
 @implementation FISAppDelegate
 
@@ -24,7 +13,8 @@
      
      */
     
-    return YES; // Don't alter this line.
-}
+    // Do not alter
+    return YES;  //
+}   ///////////////
 
 @end
